52e202600cd9a2f89e276bc7e35cded927b0ff88
Andrei Markov <noteandmark@gmail.com>
2023-08-03 17:35:40 +0300

    initial project commit

---
+++ qoutegen/src/main/java/com/andmark/qoutegen/QoutegenApplication.java
@@ -0,0 +1,19 @@
+package com.andmark.qoutegen;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class QoutegenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QoutegenApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper mapper(){
+		return new ModelMapper();
+	}
+}
---
+++ qoutegen/src/test/java/com/andmark/qoutegen/QoutegenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andmark.qoutegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QoutegenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
