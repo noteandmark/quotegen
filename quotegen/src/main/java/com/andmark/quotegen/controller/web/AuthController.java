97082fb5ac2e2699e684495cd3997bd8fd9c7972
Andrei Markov <noteandmark@gmail.com>
2023-11-29 18:41:22 +0200

    working with functionality for authorisation+registration web, day291123, draft, with variants in process login

--- quotegen/src/main/java/com/andmark/quotegen/controller/web/AuthController.java
+++ quotegen/src/main/java/com/andmark/quotegen/controller/web/AuthController.java
@@ -1,115 +1,161 @@
-//package com.andmark.quotegen.controller.web;
-//
-//import com.andmark.quotegen.dto.UserDTO;
-//import com.andmark.quotegen.service.RegistrationService;
-//import com.andmark.quotegen.service.UserService;
-//import com.andmark.quotegen.util.PersonValidator;
-//import lombok.extern.slf4j.Slf4j;
-//import org.modelmapper.ModelMapper;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.AuthenticationException;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.BindingResult;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.Map;
-//
-//@Controller
-//@RequestMapping("/auth")
-//@Slf4j
-//public class AuthController {
-//
-//    private final AuthenticationManager authenticationManager;
-//    private final ModelMapper mapper;
-//    private final PersonValidator personValidator;
-//    private final RegistrationService registrationService;
-//    private final UserService userService;
-//
-//    @Autowired
-//    public AuthController(AuthenticationManager authenticationManager, ModelMapper mapper, PersonValidator personValidator, RegistrationService registrationService, UserService userService) {
-//        this.authenticationManager = authenticationManager;
-//        this.mapper = mapper;
-//        this.personValidator = personValidator;
-//        this.registrationService = registrationService;
-//        this.userService = userService;
-//    }
-//
-//    @GetMapping("/login")
-//    public String login(@RequestParam(required = false) String error, Model model) {
-//        if (error != null) {
-//            model.addAttribute("errorMessage", "Неправильный ввод логина или пароля");
-//        }
-//        return "auth/login";
-//    }
-//
-//    @GetMapping("/registration")
-//    public String registration(@ModelAttribute("userDTO") UserDTO userDTO) {
-//        return "auth/registration";
-//    }
-//
-//    @PostMapping("/process_login")
-//    public String processLogin(@RequestParam String username,
-//                               @RequestParam String password,
-//                               Model model,
-//                               Authentication authentication) {
-//        try {
-//            log.debug("try get authentication with username = {}", username);
-//
-//            // Authenticate the user
-//            authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+package com.andmark.quotegen.controller.web;
+
+
+import com.andmark.quotegen.dto.UserDTO;
+import com.andmark.quotegen.exception.UsernameAlreadyExistsException;
+import com.andmark.quotegen.service.RegistrationService;
+import com.andmark.quotegen.service.impl.UserServiceImpl;
+import com.andmark.quotegen.util.PersonValidator;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+@RequestMapping("/auth")
+@Slf4j
+public class AuthController {
+
+    private final UserServiceImpl userService;
+    private final PersonValidator personValidator;
+    private final RegistrationService registrationService;
+    private final AuthenticationManager authenticationManager;
+
+    public AuthController(UserServiceImpl userService, PersonValidator personValidator, RegistrationService registrationService, AuthenticationManager authenticationManager) {
+        this.userService = userService;
+        this.personValidator = personValidator;
+        this.registrationService = registrationService;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @GetMapping("/login")
+    public String login(@RequestParam(required = false) String error, Model model) {
+        log.debug("get to /auth/login");
+        if (error != null) {
+            log.warn("Incorrect login or password entry");
+            model.addAttribute("errorMessage", "Неправильный ввод логина или пароля");
+        }
+        return "auth/login";
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model) {
+        log.debug("get to /auth/registration");
+        model.addAttribute("userDTO", new UserDTO());
+        return "auth/registration";
+    }
+
+    @PostMapping("/process_login")
+    public String processLogin(@Valid @RequestParam String username,
+                               @Valid @RequestParam String password,
+                               Model model) {
+        log.debug("get to /auth/process_login");
+        try {
+            log.debug("try get authentication with username = {}, password = {}", username, password);
+
+            // Authenticate the user
+//            Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 //            SecurityContextHolder.getContext().setAuthentication(authentication);
-//
-//            // Log successful authentication
-//            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-//            log.debug("Successfully authenticated. User: {}", userDetails.getUsername());
-//
+
+//            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+//                    username, password);
+//            Authentication authentication = this.authenticationManager.authenticate(token);
+//            SecurityContext context = SecurityContextHolder.createEmptyContext();
+//            context.setAuthentication(authentication);
+//            SecurityContextHolder.setContext(context);
+
+//            it worked
+//            UserDetails principal = userService.loadUserByUsername(username);
+//            Authentication authentication = new UsernamePasswordAuthenticationToken(principal, principal.getPassword(), principal.getAuthorities());
+//            SecurityContext context = SecurityContextHolder.createEmptyContext();
+//            context.setAuthentication(authentication);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(username, password);
+            Authentication authentication = authenticationManager.authenticate(authToken);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+//            Authentication authentication  =
+//                    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+
+//            Authentication authenticationRequest = UsernamePasswordAuthenticationToken.unauthenticated(username, password);
+//            Authentication authentication = this.authenticationManager.authenticate(authenticationRequest);
+
+            // Log successful authentication
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            log.debug("Successfully authenticated. User: {}", userDetails.getUsername());
+
 //            // Check roles
 //            boolean isAdmin = userService.isAdmin(authentication);
 //            boolean isUser = userService.isUser(authentication);
 //            log.debug("Role isAdmin = {}, is User = {}", isAdmin, isUser);
-//
+
 //            model.addAttribute("isAdmin", isAdmin);
 //            model.addAttribute("isUser", isUser);
-//
-//            // Redirect to the home page
-//            return "redirect:/index";
-//        } catch (AuthenticationException e) {
-//            log.warn("AuthenticationException with username = {}", username);
-//
-//            // Handle authentication errors, e.g., incorrect login/password
-//            model.addAttribute("error", "Invalid username or password");
-//            return "redirect:/auth/login?error";
-//        }
-//    }
-//
-//    @PostMapping("/registration")
-//    public String processRegistration(@ModelAttribute("userDTO") UserDTO userDTO,
-//                                      Model model,
-//                                      BindingResult bindingResult) {
-//        log.debug("registration post controller");
-//
-//        // Validate the userDTO
-//        personValidator.validate(userDTO, bindingResult);
-//
-//        log.debug("validate registration");
-//        if (bindingResult.hasErrors()) {
-//            log.warn("error validation registration");
-//            model.addAttribute("registrationError", "Ошибка в процессе регистрации. Попробуйте повторить");
-//            return "auth/registration";
-//        }
-//
-//        // Register the new user
-//        registrationService.registerNewUser(userDTO);
-//        log.debug("controller: user with username = {} registrated", userDTO.getUsername());
-//
-//        // Redirect to the home page
-//        return "redirect:/index";
-//    }
-//
-//}
+
+//            // Set authentication to session manually
+//            log.debug("before SecurityContextHolder.getContext().getAuthentication() = {}", SecurityContextHolder.getContext().getAuthentication());
+//            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("after SecurityContextHolder.getContext().getAuthentication() = {}", SecurityContextHolder.getContext().getAuthentication());
+
+            // Redirect to the home page
+            return "redirect:/public/index";
+        } catch (AuthenticationException e) {
+            log.warn("AuthenticationException with username = {}", username);
+
+            // Handle authentication errors, e.g., incorrect login/password
+            model.addAttribute("error", "Invalid username or password");
+            return "redirect:/auth/login?error";
+        }
+    }
+
+    @PostMapping("/registration")
+    public String processRegistration(@ModelAttribute("userDTO") @Valid UserDTO userDTO,
+                                      Model model,
+                                      BindingResult bindingResult,
+                                      HttpServletRequest request) {
+        log.debug("registration post controller");
+
+        // Validate the userDTO
+        personValidator.validate(userDTO, bindingResult);
+
+        log.debug("validate registration");
+        if (bindingResult.hasErrors()) {
+            log.warn("error validation registration");
+            model.addAttribute("registrationError", "Ошибка в процессе регистрации. Попробуйте повторить");
+            return "auth/registration";
+        }
+
+        // Register the new user
+        Long userId = registrationService.registerNewUser(userDTO);
+        log.debug("controller: user with id = {}, username = {} registrated", userId, userDTO.getUsername());
+
+        // Authenticate the user after registration
+        UserDetails userDetails = userService.loadUserByUsername(userDTO.getUsername());
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // Redirect to the home page
+        return "redirect:/public/index";
+    }
+
+    @ExceptionHandler(UsernameAlreadyExistsException.class)
+    public RedirectView handleUsernameAlreadyExistsException(UsernameAlreadyExistsException e, Model model,
+                                                             RedirectAttributes redirectAttributes) {
+        log.warn("Exception: {}", e.getMessage());
+        //Passing an attribute using Flash Attributes
+        redirectAttributes.addFlashAttribute("registrationError", "Пользователь с таким логином уже существует");
+        return new RedirectView("/auth/registration",true);
+    }
+
+}
